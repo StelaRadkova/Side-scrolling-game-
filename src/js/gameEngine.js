c80114354cfd89e16b8f3ede61a6072832395301
StelaRadkova <89254755+StelaRadkova@users.noreply.github.com>
2022-08-17 00:07:01 +0300

    Added game character

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -2,6 +2,5 @@ function start(state,game){
     window.requestAnimationFrame(gameLoop.bind(null,state,game))
 }
 function gameLoop(state,game){
-console.log('frame');
-window.requestAnimationFrame(gameLoop)
+window.requestAnimationFrame(gameLoop.bind(null,state,game))
 }
--- src/js/gameObjects.js
+++ src/js/gameObjects.js
@@ -1,8 +1,10 @@
 function initObjects(){
 const startScreen = document.querySelector('.start-screen');
 const gameScreen = document.querySelector('.game-screen');
+const fairy = document.querySelector('.fairy');
 return{
     startScreen,
-    gameScreen
+    gameScreen,
+    fairy
 }
 }
