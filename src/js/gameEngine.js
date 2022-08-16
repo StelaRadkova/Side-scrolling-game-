1844d1d216bb551a50c4f1dd30e3aea873710de2
StelaRadkova <89254755+StelaRadkova@users.noreply.github.com>
2022-08-16 23:17:43 +0300

    Create game engine

---
+++ src/js/gameEngine.js
@@ -0,0 +1,7 @@
+function start(state,game){
+    window.requestAnimationFrame(gameLoop.bind(null,state,game))
+}
+function gameLoop(state,game){
+console.log('frame');
+window.requestAnimationFrame(gameLoop)
+}
