d7f624a9cd72c3b93c697779df2632f7eb60be7f
StelaRadkova <89254755+StelaRadkova@users.noreply.github.com>
2022-08-18 23:12:37 +0300

    Added key registration

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,18 @@
 let state = initState();
 let game = initObjects();
-game.startScreen.addEventListener('click', (e)=> {
-       
-        game.startScreen.classList.add('hidden');
-        game.gameScreen.classList.remove('hidden');
-       start(state,game) 
+const availableKeys = ["keyA", "keyS", "keyD", "keyW"];
+document.addEventListener("keydown", (e) => {
+  if(availableKeys.includes(e.code)) {
+    state.keys[e.code] = true;
+  }
+});
+document.addEventListener("keyup", (e) => {
+  if(availableKeys.includes(e.code)) {
+    state.keys[e.code] = false;
+  }
+});
+game.startScreen.addEventListener("click", (e) => {
+  game.startScreen.classList.add("hidden");
+  game.gameScreen.classList.remove("hidden");
+  start(state, game);
 });
