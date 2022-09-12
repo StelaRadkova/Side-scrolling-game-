70c93096b52bd4ab0d9cdb6bff620071c837f608
StelaRadkova <89254755+StelaRadkova@users.noreply.github.com>
2022-09-12 13:00:10 +0300

    Add Fairy Movement boundaries

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -6,16 +6,16 @@ function gameLoop(state, game) {
   const { fairy } = state;
   const { fairyElement } = game;
   if (state.keys.KeyD) {
-    fairy.posX += fairy.speed;
+    fairy.posX = Math.min(fairy.posX + fairy.speed, game.gameScreen.offsetWidth - fairy.width);
   }
   if (state.keys.KeyA) {
-    fairy.posX -= fairy.speed;
+    fairy.posX = Math.max(fairy.posX - fairy.speed, 0);;
   }
  if (state.keys.KeyW) {
-    fairy.posY -= fairy.speed;
+    fairy.posY = Math.max(fairy.posY - fairy.speed, 0);
   }
   if (state.keys.KeyS) {
-    fairy.posY += fairy.speed;
+    fairy.posY = Math.min(fairy.posY + fairy.speed, game.gameScreen.offsetHeight - fairy.height);
   }
   fairyElement.style.left = fairy.posX + "px";
   fairyElement.style.top = fairy.posY + "px";
